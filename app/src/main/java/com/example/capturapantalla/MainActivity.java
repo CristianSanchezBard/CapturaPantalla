4413416cf2ec0d5ee421f488ed23d95e3a74703d
CristianSanchezBardo <cristiansb42@educastur.es>
2021-11-05 12:55:51 +0100

    comiteaaaaaao

--- app/src/main/java/com/example/capturapantalla/MainActivity.java
+++ app/src/main/java/com/example/capturapantalla/MainActivity.java
@@ -2,9 +2,24 @@ package com.example.capturapantalla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    static Bitmap bitmap;
+    Switch interruptor;
+    Button boton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        View v= getWindow().getDecorView().getRootView();
+        v.setDrawingCacheEnabled(true);
+        bitmap= Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false);
 
+    }
 
+    public void lanzarVentalla(){
+        Intent lanzar = new Intent(this,SegundaPantalla.class);
+        
+
+    }
 
 
 }
--- app/src/main/java/com/example/capturapantalla/SegundaPantalla.java
+++ app/src/main/java/com/example/capturapantalla/SegundaPantalla.java
@@ -3,8 +3,11 @@ package com.example.capturapantalla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SegundaPantalla extends AppCompatActivity {
+
+    ImageView imagen= findViewById(R.id.ivImagen);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
